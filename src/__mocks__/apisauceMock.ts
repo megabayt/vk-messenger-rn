d82d37b12ddeac888fb52e46b51f02aff9e73ad5
Артемий Мартьянов <artemiy@martyanov.xyz>
2019-08-07 12:36:10 +0300

    apisauce mock new implementation

--- src/__mocks__/apisauceMock.ts
+++ src/__mocks__/apisauceMock.ts
@@ -1,6 +1,7 @@
 import { IApisauceService, okResponse } from '@/utils/apisauce';
 
 export const apisauceMock: IApisauceService = {
+  setToken: jest.fn(),
   getConversations: jest.fn(async () => ({
     ...okResponse,
     data: {
